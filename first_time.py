b9b1ca338d57de26e10545de5bbab09c46193d27
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2024-04-27 20:28:21 +0200

    Update first_time.py

--- first_time.py
+++ first_time.py
@@ -2,17 +2,22 @@
 import subprocess
 import sys
 
-def install(package):
+def pip_install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install("requests")
-install("numpy")
-install("pyaudio")
-install("tflite")
-install("tflite-runtime")
-install("openwakeword")
-install("gTTS")
-install("playsound")
-install("SpeechRecognition")
+password = input("Enter your sudo password: ")
+
+pip_install("requests")
+pip_install("numpy")
+pip_install("tflite")
+pip_install("tflite-runtime")
+pip_install("openwakeword")
+pip_install("gTTS")
+pip_install("playsound")
+pip_install("SpeechRecognition")
+
+subprocess.check_call(["echo", password, "|", "sudo", "-S", "apt", "install", "-y", "portaudio19-dev"])
+
+pip_install("pyaudio")
 
 print("Setup done!")
